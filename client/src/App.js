dce13231bd932542b7f73440f0fc2fbec271b05f
Christian <christian.ninaus@stud.fh-campuswien.ac.at>
2023-12-20 23:47:01 +0100

    Show data on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [listOfThreads, setListOfThreads] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:5001/").then((response) => {
-      console.log(response);
+      setListOfThreads(response.data);
     });
   }, []);
-  return <div className="App">Welcome to CampusConnect</div>;
+  return ( 
+    <div className="App">
+      {listOfThreads.map((value, key) => { 
+      return (
+      <div className="thread">
+        <div className="title">{value.title} </div>
+        <div className="body">{value.threadText} </div>
+        <div className="footer">{value.username} </div>
+      </div> 
+      );
+    })}
+  </div>
+  );
 }
 
 export default App;
