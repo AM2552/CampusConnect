9a36ef911468b687d3dde3c40ba5c810e5fcf7a9
6856fcb <114426591+6856fcb@users.noreply.github.com>
2024-01-07 16:56:02 +0100

    Working Auth - Backend

---
+++ server/routes/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+//change this to .env process var
+const JWT_SECRET = 'jwt_secret_123';
+
+function verifyToken(req, res, next) {
+    const token = req.header('auth-token').split(' ')[1];
+    if (!token) return res.status(401).send('Access Denied');
+
+    try {
+        const verified = jwt.verify(token, JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('Invalid Token');
+    }
+}
+
+module.exports = verifyToken;
--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const db = require("./models");
 const cors = require("cors");
 const getRouter = require("./routes/threads");
 const postsRouter = require("./routes/posts");
+const users = require("./routes/users");
 
 app.use(express.json());
 app.use(cors());
 app.use("/", getRouter);
 app.use("/posts", postsRouter);
+app.use("/users", users)
 
 //goes over every table in models folder, checks if they
 //exist in the database and if not, create it
