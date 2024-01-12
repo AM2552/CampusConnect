a5bf1868ae54ab0b7df1b5634feb352a1fd0d19f
6856fcb <114426591+6856fcb@users.noreply.github.com>
2024-01-12 18:09:53 +0100

    store logged in state

--- client/src/helpers/AuthProvider.js
+++ client/src/helpers/AuthProvider.js
@@ -5,37 +5,44 @@ import axios from "axios";
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(localStorage.getItem("accessToken") || "");
-  const [authState, setAuthState] = useState(false)
-  const [modState, setModState] = useState(false)
   const navigate = useNavigate();
+
+  // Get user and modState from localStorage
+  const savedUser = localStorage.getItem("user");
+  const savedModState = localStorage.getItem("modState") === 'true';
+
+  const [user, setUser] = useState(savedUser);
+  const [token, setToken] = useState(localStorage.getItem("accessToken") || "");
+  const [authState, setAuthState] = useState(!!savedUser);
+  const [modState, setModState] = useState(savedModState);
+
   const loginAction = async (data) => {
     axios
       .post("http://localhost:5001/users/login", data)
       .then((res) => {
-      localStorage.setItem("accessToken", res.data.token.split(' ')[1]);
-      setUser(res.data.username);
-      setToken(res.data.token);
-      setAuthState(true);
-      if(res.data.mod){
-        setModState(true);
-      }
-      alert("Logged in as " + res.data.username);
-      navigate("/");
-    })
-    .catch(() => {
-      alert("Wrong Username or Password");
-    })
-    
-
+        localStorage.setItem("accessToken", res.data.token.split(' ')[1]);
+        localStorage.setItem("user", res.data.username);
+        localStorage.setItem("modState", res.data.mod);
+        setUser(res.data.username);
+        setToken(res.data.token);
+        setAuthState(true);
+        setModState(res.data.mod);
+        alert("Logged in as " + res.data.username);
+        navigate("/");
+      })
+      .catch(() => {
+        alert("Wrong Username or Password");
+      });
   };
 
   const logOut = () => {
     setUser(null);
     setToken("");
     setAuthState(false);
+    setModState(false);
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
+    localStorage.removeItem("modState");
     navigate("/");
   };
 
@@ -44,7 +51,6 @@ const AuthProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
-
 };
 
 export default AuthProvider;
