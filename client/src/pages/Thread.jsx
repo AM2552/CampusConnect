54c03c76fd31ea62c40d170fdd311f1417f317c2
lukatr1 <luka.trifunovic@stud.fh-campuswien.ac.at>
2024-01-21 13:46:35 +0100

    style changes, added confetti dependencies

--- client/src/pages/Thread.jsx
+++ client/src/pages/Thread.jsx
@@ -197,7 +197,7 @@ function Thread() {
   };
 
 
-  const renderButton = (condition, onClick, text) => condition && <button onClick={onClick}>{text}</button>;
+  const renderButton = (condition, onClick, text, className) => condition && <button className={className} onClick={onClick}>{text}</button>;
 
 
   return (
@@ -208,24 +208,33 @@ function Thread() {
           <div className={`threadObject${prop}`}>{threadObject[prop]}</div>)} 
           {threadObject.closed && <div className="threadStatus">This thread is closed.</div>}
           {threadObject.archived && <div className="threadStatus">This thread is archived.</div>}
-          {renderButton(auth.modState, deleteThread, 'Delete Thread')}
-          {renderButton(auth.modState && !threadObject.closed, closeThread, 'Close Thread')}
-          {renderButton(auth.modState && !threadObject.archived, archiveThread, 'Archive Thread')}
+          <div className="modButtons">
+          {renderButton(auth.modState, deleteThread, 'Delete Thread', "deleteThreadBtn")}
+          {renderButton(auth.modState && !threadObject.closed, closeThread, 'Close Thread', "closeThreadBtn")}
+          {renderButton(auth.modState && !threadObject.archived, archiveThread, 'Archive Thread', "archiveThreadBtn")}
+          </div>
         </div>
       </div>
       <div className="threadPageRight">
+        <div className="searchPostsMainDiv">
         <input 
           type="text" 
           placeholder="Search by user..." 
           value={searchUser} 
           onChange={handleUserSearchChange}
+          className="userSearch"
         />
+        
+        <div className="searchPostByKey">
         <input 
           type="text" 
           placeholder="Search by keyword..." 
           value={searchKeyword} 
           onChange={handleKeywordChange}
+          className="keySearch"
         />
+        </div>
+        </div>
         {!threadObject.closed && !threadObject.archived && (
           <div className="addPostField">
             <input type="text" placeholder="Post your comment..." value={newPost} onChange={e => setNewPost(e.target.value)} className="commentInput" />
@@ -237,7 +246,7 @@ function Thread() {
           <div key={key} className="post">
             <div className="postText">{post.postText}</div>
             <div className="postAuthor">{post.username}</div>
-            {renderButton((auth.modState || post.username === auth.user) && !threadObject.closed, () => removePost(post), 'Delete')}
+            {renderButton((auth.modState || post.username === auth.user) && !threadObject.closed, () => removePost(post), 'Delete' ,"deletePostBtn")}
             {post.username === auth.user && (
               <>
                 {editingPostId === post.id ? (
@@ -249,7 +258,7 @@ function Thread() {
                     <input type="text" placeholder="Updated post text..." value={updatedPost} onChange={e => setUpdatedPost(e.target.value)} />
                     <button type="submit">Submit</button>
                   </form>
-                ) : renderButton(!threadObject.closed, () => startEditing(post), 'Edit')}
+                ) : renderButton(!threadObject.closed, () => startEditing(post), 'Edit', "editPostBtn")}
               </>
             )}
           </div>
